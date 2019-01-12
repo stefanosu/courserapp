4454a21dd7161b7eb050baf6ac8910757b548093
stefanosu <stefanosu422@gmail.com>
2019-01-11 23:04:58 -0500

    Components part 2

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,63 +1,52 @@
 import React, {Component} from 'react';
-import {Media} from 'reactstrap';
+import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap';
 
 class Menu extends Component {
     constructor(props) {
         super(props);
        //this.state stores properties related to this component that we can make use of
         this.state = {
-            dishes: [
-                {
-                  id: 0,
-                  name:'Uthappizza',
-                  image: 'assets/images/uthappizza.png',
-                  category: 'mains',
-                  label:'Hot',
-                  price:'4.99',
-                  description:'A unique combination of Indian Uthappam (pancake) and Italian pizza, topped with Cerignola olives, ripe vine cherry tomatoes, Vidalia onion, Guntur chillies and Buffalo Paneer.'                        },
-               {
-                  id: 1,
-                  name:'Zucchipakoda',
-                  image: 'assets/images/zucchipakoda.png',
-                  category: 'appetizer',
-                  label:'',
-                  price:'1.99',
-                  description:'Deep fried Zucchini coated with mildly spiced Chickpea flour batter accompanied with a sweet-tangy tamarind sauce'                        },
-               {
-                  id: 2,
-                  name:'Vadonut',
-                  image: 'assets/images/vadonut.png',
-                  category: 'appetizer',
-                  label:'New',
-                  price:'1.99',
-                  description:'A quintessential ConFusion experience, is it a vada or is it a donut?'                        },
-               {
-                  id: 3,
-                  name:'ElaiCheese Cake',
-                  image: 'assets/images/elaicheesecake.png',
-                  category: 'dessert',
-                  label:'',
-                  price:'2.99',
-                  description:'A delectable, semi-sweet New York Style Cheese Cake, with Graham cracker crust and spiced with Indian cardamoms'                        }
-               ],
-        };
+          selectedDish:null
+        }
+    }
+
+
+    onDishSelect(dish){
+      this.setState({ selectedDish: dish});
+    }
+
+    renderDish(dish){
+      if(dish != null){
+          return(
+          <Card>
+            <CardImg width="100%"  src={dish.image} alt={dish.name} />
+            <CardBody>
+              <CardTitle>{dish.name}</CardTitle>
+              <CardText>{dish.description}</CardText>
+            </CardBody>
+          </Card>
+          );
+      }
+      else {
+        return(
+            <div></div>
+          );
+      }
     }
 
     render() {
-        const menu = this.state.dishes.map((dish) => {
+       
+        const menu = this.props.dishes.map((dish) => {
             return (
  // Whenever you construct a list of items in React every item requires a key attribute to be specified for it.
  // Because the key helps when React is rendering these items on the screen the key helps react to recongize each one of these elements so the key will enable it to identify each of these elements uniquely when it sees that it needs to update the UI in case more items are added to the list 
-              <div key={dish.id} className="col-12 mt-5">
-                <Media tag="li">
-                  <Media left middle>
-                      <Media object src={dish.image} alt={dish.name} />
-                  </Media>
-                  <Media body className="ml-5">
-                    <Media heading>{dish.name}</Media>
-                    <p>{dish.description}</p>
-                  </Media>
-                </Media>
+              <div key={dish.id} className="col-12 col-md-5 m-1">
+                <Card onClick={() => this.onDishSelect(dish)}>
+                      <CardImg width="100%"  src={dish.image} alt={dish.name} />
+                  <CardImgOverlay>
+                    <CardTitle>{dish.name}</CardTitle>
+                  </CardImgOverlay>
+                </Card>
               </div>
             );
         });
@@ -65,10 +54,11 @@ class Menu extends Component {
         return (
           <div className="container">
             <div className="row">
-              <Media list>
                   {menu}
-              </Media>
             </div>
+            <div className="row">
+              {this.renderDish(this.state.selectedDish)}
+             </div> 
           </div>
         );
     }
